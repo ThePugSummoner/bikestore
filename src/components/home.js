d6b4779c6ac333b75201e8f5026dd724a0fad241
TomiV <tomi.valtanen1991@gmail.com>
2022-11-10 20:13:01 +0200

    fixed

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,11 @@
 import React from "react"
 import Banner from "./banner"
-import Card from "./card"
 import ImageFrame from "./imagaFrame"
 import LogoList from "./logolist"
 
 function Home(){
 
 
-<<<<<<< HEAD
-for (let i=0; i<10; i++){
-   array.push(<li><Card/></li>)
-}
-=======
->>>>>>> 80f14d02e256595893c14d30b4b9ff5993537d03
 
 
     return(
