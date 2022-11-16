b8a79538b3ec616f16b054e267cc033d29c41f36
Ilia Zubov <97738404+ThePugSummoner@users.noreply.github.com>
2022-11-16 19:21:56 +0200

    Fontawesome added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import Account from './components/account';
 import axios from 'axios';
 import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCheckSquare, faCoffee)
+library.add (faShoppingCart, faUser)
 
 
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,13 +4,18 @@ import cart from "../images/shoppingcart icon.png"
 import logo from "../images/AngularBikes3.png"
 import { Link } from "react-router-dom"
 import searchLogo from "../images/spanner 2.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faShoppingCart, faWrench } from '@fortawesome/free-solid-svg-icons'
+
 
 
 const linkStyle = {
     margin: "1rem",
     textDecoration: "none",
     color: 'black',
+    textAlign: 'center'
 };
+
 
 function Navbar() {
 
@@ -19,14 +24,16 @@ function Navbar() {
             <div className="top-nav">
             <Link  to="/"><img className="logo-img"  src={logo} alt="logo"></img></Link>
             <input className="top-nav-search"  type="text" placeholder="Search..."></input>
-            <button className="search-button"><img src={searchLogo} alt="search logo"></img></button>
+            <button className="search-button"> <FontAwesomeIcon icon={faWrench} size="2x"/></button>
             <div className="user"> 
-            <img className="user-logo" src={userIcon} alt="usericon"></img>          
-            <Link style={linkStyle} to="/userinfo">Oma Tili</Link>
+            {/*<img className="user-logo" src={userIcon} alt="usericon"></img>*/}
+                        
+                <Link style={linkStyle} to="/userinfo"><FontAwesomeIcon icon={faUser} size="2x" />KIRJAUDU SISÄÄN</Link>
             </div>  
             <div className="shoppingCart"> 
-            <img className="cart-logo" src={cart} alt="cartlogo"></img>        
-            <Link style={linkStyle} to="/shoppingcart">Ostoskärry</Link>
+            {/*<img className="cart-logo" src={cart} alt="cartlogo"></img>*/}
+                
+                <Link style={linkStyle} to="/shoppingcart"><FontAwesomeIcon icon={faShoppingCart} size="2x"/>OSTOSKORI</Link>
             </div>
             </div>
             <div className="bottom-nav">
