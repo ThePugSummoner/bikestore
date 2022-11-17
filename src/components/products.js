276abab2297262d20fe85aeee825943f8fecf4d1
Jealbepi <jere.pihlajasaari@hotmail.com>
2022-11-17 20:11:26 +0200

    Ostoskorin koodausta ja ulkonäköä

---
+++ src/components/products.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        id: 1,
+        name: "Scott Navajo 955 HD 22",
+        desc: "Maastopyörä",
+        price: 999,
+        image: ""
+    },
+    {
+        id: 2,
+        name: "Cube Analog HD RLO SX12 22",
+        desc: "Maastopyörä unisex", 
+        price: 759,
+        image: ""
+    },
+];
+
+module.exports = products
--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+import productsReducer, {productsFetch} from "./features/productsSlice";
+import {productsApi} from "./features/productsAPI"
+import cartReducer, {getTotals} from './features/cartSlice';
+
+const store = configureStore({
+  reducer: 
+    {products: productsReducer, 
+      cart: cartReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
+});
+
+store.dispatch(productsFetch());
+store.dispatch(getTotals());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
   </BrowserRouter>
 );
