9234da7ac812a1a44e1cee4dd4cd6864d6194f21
TomiV <tomi.valtanen1991@gmail.com>
2022-12-14 18:43:54 +0200

    fix

--- src/components/categoryPage/category.js
+++ src/components/categoryPage/category.js
@@ -6,6 +6,8 @@ import uuid from 'react-uuid'
 import { Link } from "react-router-dom"
 import Card from "../card/card"
 import "./category.css"
+import { useSelector, useDispatch } from "react-redux";
+import { getTotals } from '../../features/cartSlice';
 
 const URL = 'http://localhost/angularbikes/'
 
